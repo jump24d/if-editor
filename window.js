9b231fc66dac787ad318f1a8812dc86d8f202cea
yanc <jump24d@gmail.com>
2022-08-21 22:21:20 +0900

    add kbn edit feature

--- window.js
+++ window.js
@@ -4,13 +4,23 @@ $(() => {
     const text = this.value
 
     const kbn = text.substring(0,1)
-    $('#kbn').text(kbn)
+    $('#kbn').val(kbn)
 
     const uriage = text.substring(2,6)
     $('#uriage').text(uriage)
 
     const id = text.substring(7,9)
     $('#id').text(id)
+
+    $("#result").text(text)
+  })
+
+  $('#kbn').bind('input propertychange', function() {
+    const text = this.value
+
+    const kbn = text.substring(0,1) 
+    const fullText = $('#text-input').val()
+    $('#result').text(kbn + fullText.substring(1))
 
   })
 
